a33e9464c4043a032e209b42fb0e182c0b1d61ca
IJD <ijd26@case.edu>
2024-12-26 23:43:09 -0500

    return to jekyll :(

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/{i-Dyke.github.io}' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/{i-Dyke.github.io}' : '',
-  }
-  
-  module.exports = nextConfig
